309402a0de7d7a8ab4d1d204b890c456bd40ae01
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-08-11 19:58:25 +0200

    add client to experss [21.6]

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./db');
 const cors = require('cors')
+const path = require('path');
 
 const app = express();
 
@@ -12,14 +13,20 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/NewWaveFest/build')));
 app.use('/api', testimonialsRoutes); // add testimonials routes to server
 app.use('/api', concertsRoutes); // add concerts routes to server
 app.use('/api', seatsRoutes); // add seats routes to server
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/NewWaveFest/build/index.html'));
+});
  
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
 
-app.listen(8000, () => {
+
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
