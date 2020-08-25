76a3672d46902651a756d64bfcd50d7b1a4d2cb2
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-08-25 20:56:03 +0200

    add MongoDB Atlas [23.6]

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use((req, res) => {
 })
 
 // connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://Agath-e:<password>@cluster0.vnu2y.gcp.mongodb.net/NewWaveDB?retryWrites=true&w=majority', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
